5d9c81b010abb5d6e4898756b70128209afb5f74
1315groop <pashakorsh@gmail.com>
2021-08-11 09:54:16 +0300

    add makemigrations

--- simple_todo/db_populate.py
+++ simple_todo/db_populate.py
@@ -1,23 +1,9 @@
-import psycopg2
 from factory import Faker
 from factory.django import DjangoModelFactory
 
-conn = psycopg2.connect(
-    database="simpletodo_database", user="simpletodo_user", password="p5cLW7lvmC5Ep50j", host="postgres", port="5432"
-)
-print ("Opened database successfully")
-cur = conn.cursor()
+from core.models import Post
 
-for i in range(10):
-    Id = fake.random_digit_not_null()
-    name = fake.name()
-    age = fake.random_number(digits=None)
-    adress = fake.address()
-    salary = fake.random_int(min=0, max=9999)
-    cur.execute(
-        "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) VALUES (%s, %s, %s, %s, %s)", (Id, name, age, adress, salary)
-    )
 
-conn.commit()
-print "Records created successfully"
-conn.close()
+class AccountFactory(DjangoModelFactory):
+    class Meta:
+        model = Post
